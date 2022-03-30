ca26a686c838ba3a5c47a66adf280ce1212e59f0
Yashlohade <73010631+yashlohade21@users.noreply.github.com>
2022-03-30 19:58:10 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
       <div className="a-right">
         <h1 className="a-title">About Me</h1>
         <p className="a-sub">
-        I am a Software Engineer with a 2 Years Experience in PHP, Html 5, Bootstrap, MySQL, Laravel, WordPress, E commerce Website And Reactjs. I develop responsive websites  . I can develop any kind of Web Applications and Website. If you are looking for a professional who can handle your work elegantly then I am here with my all skills. Please do contact me to share your projects.
+        I am a Computer Engineer with a 2 Years Experience in PHP, Html 5, Bootstrap, MySQL, Laravel, WordPress, E commerce Website And Reactjs. I develop responsive websites  . I can develop any kind of Web Applications and Website. If you are looking for a professional who can handle your work elegantly then I am here with my all skills. Please do contact me to share your projects.
         </p>
       </div>
     </div>
