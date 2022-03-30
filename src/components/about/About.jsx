9b897c33c461d63d8794df075a29c04acc211bd2
Yashlohade <73010631+yashlohade21@users.noreply.github.com>
2022-03-30 19:57:30 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,38 +17,8 @@ const About = () => {
       <div className="a-right">
         <h1 className="a-title">About Me</h1>
         <p className="a-sub">
-        I am a Software Engineer with a 8 Years Professional Experience in PHP, Html 5, Bootstrap, MySQL, Laravel, WordPress, E commerce. I develop both web based and desktop based software. I can develop any kind of Web Applications and Website. If you are looking for a professional who can handle your work elegantly then I am here with my all skills. Please do contact me to share your projects.
+        I am a Software Engineer with a 2 Years Experience in PHP, Html 5, Bootstrap, MySQL, Laravel, WordPress, E commerce Website And Reactjs. I develop responsive websites  . I can develop any kind of Web Applications and Website. If you are looking for a professional who can handle your work elegantly then I am here with my all skills. Please do contact me to share your projects.
         </p>
-        <p className="a-desc">
-        PHP, Html5, Bootstrap, Mysql, Laravel, WordPress, Ecommerce
-
-I will develop, design and fix Php, Mysql, Bootstrap, Laravel and WordPress Webapps and Website. I have a professional Experience more than 8 Years, designed number of Web Application and Websites, I have also created and designed numbers of E-Commerce websites and other customized web applications for my customers. If you are looking for someone professional here I am. 
-
-I'm open to projects in the following areas:
-
-Web applications
-Dashboards
-CMS
-Custom Softwares
-PHP MySql
-Database projects
-Wordpress
-Web Development
-Query writing
-Query Optimization
-Website deployment/hosting
-
-
-Customer Satisfaction is my online concern, Clear communication is key for the 100% quality, If you have something to develop then do contact me.
-
-
-
-Have a Nice Day!
-
-
-
-PHP, Html5, Bootstrap, Mysql, Laravel, WordPress, Ecommerce
-       </p>
       </div>
     </div>
   );
